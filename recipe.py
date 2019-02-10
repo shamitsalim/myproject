1690415023d6c6a3b4f1bebbd7a8bd2c9a52c97f
shamitsalim <shamisalim888@gmail.com>
2019-02-09 21:58:12 -0800

    this extracts preparation time of recipe

--- recipe.py
+++ recipe.py
@@ -12,3 +12,4 @@ class RecipeScraper(scrapy.Spider):
 		enc=" "
 		index=response.css('h1.gel-trafalgar.content-title__text::text').extract_first()
 		image=response.css('img.recipe-media__image'). xpath('@src').extract()
+		prep_time=response.css('p.recipe-metadata__prep-time::text').extract_first()
